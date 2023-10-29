c0419749574b58f25530a3268e508f7b5d51533d
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-10-29 09:07:02 +0600

    services and booking related API formulated

---
+++ __jwt__steps.js
@@ -0,0 +1,14 @@
+/**
+ * install jsonwebtoken
+ * jwt.sign(payload, secret, {expiresIn:})
+ * token client
+ * 
+ */
+
+/**
+ * how to store token in the client side
+ * 1. memory --> ok type
+ * 2. localStorage --> ok type (XSS: cross site scripting)
+ * 3. cookies --> http only
+ * 
+ */
